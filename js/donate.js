10108bf1254e68f393d41a2fb04eac5a06ef206f
Unknown <ctran049@west-mec.org>
2018-08-24 15:25:05 -0700

    Added validation

--- js/donate.js
+++ js/donate.js
@@ -1,3 +1,4 @@
+/*
 $("document").ready(function() {
   $("#donate").click(validate);
 });
@@ -102,3 +103,111 @@ function luhn(idNum) {
     // Return whether the sume of the digits is divisible by 10
     return sumDigits(string1 + string2) % 10 === 0;
 }
+/*
+
+/*
+Project 01_06_01
+
+Author: Charley Tran
+Date: 8.16.18
+
+Filename: script.js
+*/
+
+"use strict";
+
+var formValidity = true;
+
+// This function goes through each input box to make sure that there is some content. If there isn't any content, then the input box background color will change to let the user know that they forgot to enter the input box. There will also be a message letting the user know they forgot to enter information.
+function validateRequired() {
+  var inputElements = document.getElementsByTagName("input");
+  var selectElements = document.getElementsByTagName("select");
+  var errorDiv = document.getElementById("errorText");
+  var fieldsetValidity = true;
+  var currentElement;
+
+  // This loops through the input boxes
+//   try {
+//     for (var i = 0; i < inputElements.length; i++) {
+//       currentElement = inputElements[i];
+//       if (currentElement.value === "") {
+//         currentElement.style.background = "rgb(255,233,233)";
+//         fieldsetValidity = false;
+//       } else {
+//         currentElement.style.background = "white";
+//       }
+//     }
+
+  try {
+    // loop through input fields looking for blanks
+    for (var i = 0; i < inputElements.length; i++) {
+      currentElement = inputElements[i];
+      if (currentElement.value === "") {
+        currentElement.style.background = "rgb(255, 233, 233)";
+        fieldsetValidity = false;
+      }
+      else {
+        currentElement.style.background = "white";
+      }
+    }
+
+    // validate select list field
+    for (var i = 0; i < selectElements.length; i++) {
+      currentElement = selectElements[i];
+      if (currentElement.value === "") {
+        currentElement.style.border = "2px solid red";
+        fieldsetValidity = false;
+      } 
+      else {
+        currentElement.style.border = "";
+      }
+    }
+
+     // This throws the error message
+         if (fieldsetValidity === false) {
+           throw "Please complete the rest of the form.";
+         } else {
+           errorDiv.style.display = "none";
+           errorDiv.innerHTML = "";
+           formValidity = true;
+         }
+         // This catches and displays the error message
+       } catch (msg) {
+         errorDiv.style.display = "block";
+         errorDiv.innerHTML = msg;
+         formValidity = false;
+       }
+       if (formValidity === true) {
+         document.getElementsByTagName("form")[0].submit();
+       }
+     }
+
+// Loads the above function to test for validity.
+function validateForm(evt) {
+  if (evt.preventDefault) {
+    evt.preventDefault();
+  } else {
+    evt.returnValue = false;
+  }
+
+  validateRequired();
+}
+
+function createEventListeners() {
+  var form = document.getElementsByTagName("form")[0];
+  if (form.addEventListener) {
+    form.addEventListener("submit", validateForm, false);
+  } else if (form.attachEvent) {
+    form.attachEvent("onsubmit", validateForm);
+  }
+}
+
+function reloadPage(){
+  document.location.reload();
+}
+
+// Added event listener to load the function createEventListeners.
+window.addEventListener("load", createEventListeners);
+
+var resetButton = document.getElementById("reset");
+resetButton.addEventListener("click", reloadPage, false);
